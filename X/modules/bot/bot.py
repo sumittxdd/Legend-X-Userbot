9005d92da580829c01c468cf2942261051f891d7
sumittxdd <166435710+sumittxdd@users.noreply.github.com>
2024-04-26 14:37:12 +0530

    bot.py

--- X/modules/bot/bot.py
+++ X/modules/bot/bot.py
@@ -61,7 +61,7 @@ async def on_plug_in_cb(_, callback_query: CallbackQuery):
     this_command = f"──「 **Help For {str(modul_name).upper()}** 」──\n\n"
     for x in commands:
         this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-    this_command += "© @Nobitaa_xd"
+    this_command += "© @Sumit_Xz"
     bttn = [
         [InlineKeyboardButton(text="•─╼⃝𖠁 ʀᴇᴛᴜʀɴ 𖠁⃝╾─•", callback_data="reopen")],
     ]
